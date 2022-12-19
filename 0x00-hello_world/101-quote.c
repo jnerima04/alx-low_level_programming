c1a73d3f8dc65319d1a58ede803f2d1d699e2a0b
jnerima04 <jemimanerima@gmail.com>
2022-12-19 08:50:17 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/**
+ *
+ * * main - Entry point
+ *
+ * *
+ *
+ * * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
 
 int main(void)
+
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
-	return (0);
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 
 }
