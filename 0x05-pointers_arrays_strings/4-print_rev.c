83b22c745dd79a35bdb457301eb5e0823a4c0cd7
jnerima04 <jemimanerima@gmail.com>
2022-12-19 10:19:40 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev prints the string in reverse
+ * @s: value input to be acted on
+ * Return: 0
+*/
+
+void print_rev(char *s)
+
+{
+
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+
+	{
+
+	}
+
+	for (len = len - 1; len >= 0; len--)
+
+	{
+
+		_putchar(s[len]);
+
+	}
+
+	_putchar('\n');
+
+}
