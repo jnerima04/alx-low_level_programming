b7ffb52279b99ff52876dd8219c08a059dad68f7
jnerima04 <jemimanerima@gmail.com>
2022-12-20 08:42:42 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet
+ * Return: 0
+*/
+
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+
+}
