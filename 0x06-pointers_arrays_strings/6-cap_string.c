1b2a04e0dc64fcc97ce72abb1783b6aaa830e33f
jnerima04 <jemimanerima@gmail.com>
2022-12-22 11:48:12 -0800

    Task 6.0

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,6 +5,7 @@
  * @n: input value
  * Return: string
 */
+
 char *cap_string(char *n)
 {
 	int i;
@@ -15,6 +16,7 @@ char *cap_string(char *n)
 		n[0] = n[0] - 32;
 	}
 	for (i = 0; n[i] != '\0'; i++)
+	{
 		switch (n[i])
 		{
 			case ',':
